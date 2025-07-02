75a28b58a13457b5637e3b5f4f3a2ace3c052468
Javier Echeverria Saralegui <saralegui@MacBook-Pro-de-Javier-3.local>
2025-07-02 22:24:05 +0200

    Primera versión limpia

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "static",
+  site: "https://tridecoder.github.io/20d/",
+  base: "/20d/",
+});
