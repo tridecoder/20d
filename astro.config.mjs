c9ed3a3f19cbf9e582afef7636979e49d3c10fdf
Javier Echeverria Saralegui <saralegui@MacBook-Pro-de-Javier-3.local>
2025-07-04 11:10:30 +0200

    feat: integración con React, Tailwind y Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   output: "static",
   site: "https://20d.es",
+  integrations: [react(), tailwind()],
 });
